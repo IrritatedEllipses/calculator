2c356a6d3a0acfe4d88b073ab729f082f5c87e54
Christopher Culp <Christopher.D.Culp@gmail.com>
2018-03-22 18:56:46 -0400

    Added CSS, changed structure of JS and set classes in HTML

--- js/script.js
+++ js/script.js
@@ -1,28 +1,48 @@
 var operator = ""
 var currentNum = ""
 var newNum = ""
+var calcMain = document.querySelector('.calculator')
 
-function math(operator) {
+// Take in a number
+// Make sure to take in multiple numbers
+calcMain.addEventListener('click', function(event){
+    if(event.target.id ===)
+})
 
+// Take in an operation for the number
+// If an operator is pressed twice, ignore the second press
 
-    switch (operator) {
-        case "add":
-            result = currentNum + newNum;
-            break;
+// Take in a second number
 
-        case "subtract":
-            result = currentNum - newNum;
-            break;
-
-        case "multiply":
-            result = currentNum * newNum;
-            break;
-
-        case "division":
-            result = currentNum / newNum;
-            break;
-
-
-
-    }
+// Perform operation on both numbers
+function add(a, b) {
+    return a + b
 }
+
+function sub(a, b) {
+    return a - b
+}
+
+function mult(a, b) {
+    return a * b
+}
+
+function divide(a, b) {
+    return a / b
+}
+// Disallow dividing by zero
+
+// Display results
+// Display only to the 8th decimal place
+
+// Clear the number 
+
+// Disallow dividing by Zero
+
+
+
+
+
+calcMain.addEventListener('click', function(event){
+    if(event.target.id ===)
+})
